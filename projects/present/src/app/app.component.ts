efc4b1f4a7939c45dc9ec0d657ff346afbafe8bf
Samuel Bio <biosamuel7@gmail.com>
2024-07-11 13:46:33 +0200

    remove comment

--- projects/present/src/app/app.component.ts
+++ projects/present/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  style: ''
+  styles: ''
 })
 export class AppComponent {
   title = 'present';
